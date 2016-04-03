daf1c3517d91c646c9ef49aa4dc0091b3b4ccddf
Ivan Kolev <ikolev94@yahoo.bg>
2016-04-03 23:23:43 +0300

    add bootstrap (bower.json) and add login

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('issueTrackerSystem', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'login'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '#/'});
 }]);
